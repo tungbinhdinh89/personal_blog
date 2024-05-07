f2a29e99c323f1b16a2dbe90d06c3d874d29eb23
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2024-05-07 14:24:20 +0700

    create header section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,18 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  plugins: [
+    // ...
+    flowbite.plugin(),
+  ],
 };
