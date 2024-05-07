60de12519d6fd5c2fccdf345980287d3ecfa90dc
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2024-05-07 14:27:08 +0700

    Revert "create pages and routes"
    This reverts commit 5be30f2b63814489c095f9981af4859b0b12305e.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,19 +5,17 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
-import Header from "./componets/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/dashbord" element={<Dashboard />} />
-        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<SignIn />} />
+        <Route path="/about" element={<SignUp />} />
+        <Route path="/about" element={<Dashboard />} />
+        <Route path="/about" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   );
